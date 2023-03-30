419195e1af4edcdf5968dda1b164001180f79f9b
Veli Ertunç <ertuncveli@gmail.com>
2023-03-30 08:44:02 +0300

    Restored files

---
+++ DesignPatternsCPP.h
@@ -0,0 +1,8 @@
+﻿// DesignPatternsCPP.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include "Builder.h"
+
+// TODO: Reference additional headers your program requires here.
